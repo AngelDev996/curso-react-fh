790ea639ae809497cd4592ae5247c77b719debe1
Angel Valladares <arivalladares2.0@gmail.com>
2022-10-13 13:15:28 -0600

    fragments

---
+++ 03-counter-app/src/PrimeraApp.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const PrimeraApp = () => {
+
+    return (
+            <>
+                <h1> Hola mundo </h1>
+                <p>Adios mundo</p>
+            </>
+    
+    )
+
+}
+
+
+export default PrimeraApp
