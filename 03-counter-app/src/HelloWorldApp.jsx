af071e9b5595e6c2abf726770476a727c139b7ff
Angel Valladares <arivalladares2.0@gmail.com>
2022-10-30 21:53:22 -0600

    primer componente

---
+++ 03-counter-app/src/HelloWorldApp.jsx
@@ -0,0 +1,13 @@
+// export function App(){
+//     return (<h1>Hola mundo</h1>)
+// }
+
+
+import React from 'react'
+
+export const HelloWorldApp = () => {
+  return (
+    <h1>HelloWorldApp</h1>
+  )
+}
+
--- 03-counter-app/src/main.jsx
+++ 03-counter-app/src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+
+import {HelloWorldApp} from './HelloWorldApp'
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+    <React.StrictMode>
+         <HelloWorldApp/>
+    
+    </React.StrictMode>
+
+
 )
